465ac6036ddfee065e0f4b4f945c7895ce2c3ecf
suecodingm <susana.mendoza.neira@gmail.com>
2026-05-21 17:57:10 +0000

    update terraform infrastructure and gitignore

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,7 +1,59 @@
-resource "aws_s3_bucket" "terraform_test" {
-  bucket = "tfm-test-bucket-firstattempt"
+resource "aws_security_group" "k3s_sg" {
+  name        = "k3s-security-group"
+  description = "Allow SSH and Kubernetes traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "k3s_server" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t3.small"
+  key_name = "tfm-k3s-key"
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              curl -sfL https://get.k3s.io | sh -
+              EOF
 
   tags = {
-    Name = "TerraformTest"
+    Name = "k3s-server"
   }
 }
